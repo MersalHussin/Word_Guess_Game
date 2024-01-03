3da5be06902fd4f1d466faa1422ab7a457b5aace
Mersal <123738353+MersalHussin@users.noreply.github.com>
2024-01-03 08:19:40 -0800

    Add files via upload

---
+++ Word Guess Game/main.js
@@ -0,0 +1,4 @@
+let gameName = "Guess The Word"
+document.title = gameName
+document.querySelector("h1").innerHTML = gameName
+document.querySelector("footer") .innerHTML = `${gameName} Created By <a href="#" class="link">Mersal</a>`
